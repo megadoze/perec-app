9dec607d3400379288d75aeec34cc981c5d95359
Oleg <Oleg@macs-MacBook-Pro-4.local>
2025-04-07 16:32:56 +0200

    change env

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -1,11 +1,11 @@
 // lib/firebase.js
-import { initializeApp } from 'firebase/app';
-import { getDatabase, ref, get, child } from 'firebase/database';
+import { initializeApp } from "firebase/app";
+import { getDatabase, ref, get, child } from "firebase/database";
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DB_URL,
+  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
 };
 
