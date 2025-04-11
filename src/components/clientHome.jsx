182acc13696e1ed4e31bfe9cb336b8acf399a4b2
Oleg <Oleg@macs-MacBook-Pro-4.local>
2025-04-12 00:53:54 +0200

    speed optimisation

--- src/components/clientHome.jsx
+++ src/components/clientHome.jsx
@@ -96,7 +96,8 @@ export default function ClientHome({ initialNews }) {
     <AnimatePresence mode="wait">
       <motion.div
         key={news.length} // переключатель на изменение массива
-        initial={{ opacity: 0 }}
+        // initial={{ opacity: 0 }}
+        initial={false}
         animate={{ opacity: 1 }}
         exit={{ opacity: 0 }}
         transition={{ duration: 0.4 }}
