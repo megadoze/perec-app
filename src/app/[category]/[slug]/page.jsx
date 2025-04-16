e78dc1382aad8a76132bf4452b42c5696db92df7
Oleg <Oleg@macs-MacBook-Pro-4.local>
2025-04-16 17:29:17 +0200

    schema

--- src/app/[category]/[slug]/page.jsx
+++ src/app/[category]/[slug]/page.jsx
@@ -53,5 +53,38 @@ export default async function NewsPage(props) {
     return notFound();
   }
 
-  return <NewsContent data={data} />;
+  const schema = {
+    "@context": "https://schema.org",
+    "@type": "NewsArticle",
+    headline: data.title,
+    image: [data.images?.[0] || "default-image-url"],
+    datePublished: new Date(data.createdAt).toISOString(),
+    dateModified: new Date(data.updatedAt || data.createdAt).toISOString(),
+    author: {
+      "@type": "Person",
+      name: data.author || "PEREC.news",
+    },
+    publisher: {
+      "@type": "Organization",
+      name: "PEREC.news",
+      logo: {
+        "@type": "ImageObject",
+        url: "https://perec-app.vercel.app/logoperec.png",
+      },
+    },
+    mainEntityOfPage: {
+      "@type": "WebPage",
+      "@id": `https://perec-app.vercel.app/${data.category}/${data.slug}`,
+    },
+  };
+
+  return (
+    <>
+      <script
+        type="application/ld+json"
+        dangerouslySetInnerHTML={{ __html: JSON.stringify(schema) }}
+      />
+      <NewsContent data={data} />
+    </>
+  );
 }
