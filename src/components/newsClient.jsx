c7b06b020588d15b6714b6d7c37840cca24e778b
Oleg <Oleg@macs-MacBook-Pro-4.local>
2025-04-16 16:24:57 +0200

    schema

--- src/components/newsClient.jsx
+++ src/components/newsClient.jsx
@@ -1,6 +1,7 @@
 "use client";
 
 import Image from "next/image";
+import Head from "next/head";
 import { motion } from "framer-motion";
 import BackButton from "./backButton";
 import dayjs from "dayjs";
