764d73ac75d7c63175071f2618dfaa7eba7b223f
Oleg <Oleg@macs-MacBook-Pro-4.local>
2025-04-16 19:29:44 +0200

    change logo to text 2AM

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,6 +1,5 @@
 "use client";
 
-// import dynamic from "next/dynamic";
 import Link from "next/link";
 import BurgerMenu from "./burgerMenu";
 
@@ -8,7 +7,10 @@ export default function Header() {
   return (
     <header className="h-[64px] px-4 md:px-8 py-4 flex items-center justify-between text-sm">
       <Link href="/" className="text-xl lg:text-2xl font-blackout ">
-        <img src="/logo.png" alt="perec logo" className=" w-20 md:w-24" />
+        {/* <img src="/logo.webp" alt="perec logo" className=" w-20 md:w-24" /> */}
+        <span className=" text-3xl md:text-4xl font-blackout text-red-500 tracking-tight">
+          PEREC
+        </span>
       </Link>
       <BurgerMenu />
     </header>
