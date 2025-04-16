e95562fc502daf0f377d1e78d19966f6cd597dc9
Oleg <Oleg@macs-MacBook-Pro-4.local>
2025-04-16 11:12:37 +0200

    logo

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -8,7 +8,7 @@ export default function Header() {
   return (
     <header className="h-[64px] px-4 md:px-8 py-4 flex items-center justify-between text-sm">
       <Link href="/" className="text-xl lg:text-2xl font-blackout ">
-        <img src="/logo.png" alt="" className=" w-20 md:w-24" />
+        <img src="/logo3.png" alt="" className=" w-20 md:w-24" />
       </Link>
       <BurgerMenu />
     </header>
