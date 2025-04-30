c96bae139005bcc8caf5bcf2bc28402cf7e0b9d3
Oleg <Oleg@macs-MacBook-Pro-4.local>
2025-04-30 12:26:43 +0200

    time zone

--- src/components/publishedAt.jsx
+++ src/components/publishedAt.jsx
@@ -1,4 +1,3 @@
-import { useMemo } from "react";
 import dayjs from "dayjs";
 import utc from "dayjs/plugin/utc";
 import timezone from "dayjs/plugin/timezone";
@@ -11,13 +10,21 @@ export default function PublishedAt({
   timestamp,
   format = "DD MMM YYYY HH:mm",
 }) {
-  const userTimeZone = useMemo(() => {
-    return Intl.DateTimeFormat().resolvedOptions().timeZone;
-  }, []);
-
-  const formattedDate = useMemo(() => {
-    return dayjs(timestamp).tz(userTimeZone).format(format);
-  }, [timestamp, userTimeZone, format]);
-
+  const formattedDate = dayjs(timestamp).tz("Europe/Moscow").format(format);
   return <span>{formattedDate}</span>;
 }
+
+// export default function PublishedAt({
+//   timestamp,
+//   format = "DD MMM YYYY HH:mm",
+// }) {
+//   const userTimeZone = useMemo(() => {
+//     return Intl.DateTimeFormat().resolvedOptions().timeZone;
+//   }, []);
+
+//   const formattedDate = useMemo(() => {
+//     return dayjs(timestamp).tz(userTimeZone).format(format);
+//   }, [timestamp, userTimeZone, format]);
+
+//   return <span>{formattedDate}</span>;
+// }
