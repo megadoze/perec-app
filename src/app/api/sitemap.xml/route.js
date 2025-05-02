bea76ecec9ef0e5aa5858c9138adbc0a6fe5e144
Oleg <Oleg@macs-MacBook-Pro-4.local>
2025-05-02 10:47:44 +0200

    sm

--- src/app/api/sitemap.xml/route.js
+++ src/app/api/sitemap.xml/route.js
@@ -1,30 +1,38 @@
 export async function GET() {
-  const sitemapUrl =
-    "https://storage.googleapis.com/perec-news.firebasestorage.app/sitemap-index.xml";
-
-  try {
-    const response = await fetch(sitemapUrl);
-
-    if (!response.ok) {
-      return new Response(
-        `Fetch failed: ${response.status} ${response.statusText}`,
-        {
-          status: response.status,
-        }
-      );
-    }
-
-    const xml = await response.text();
-
-    return new Response(xml, {
-      headers: {
-        "Content-Type": "application/xml",
-        "Cache-Control": "no-cache",
-      },
-    });
-  } catch (error) {
-    return new Response("Error: " + error.message, {
-      status: 500,
-    });
-  }
+  return new Response("Sitemap stub OK", {
+    headers: {
+      "Content-Type": "text/plain",
+    },
+  });
 }
+
+// export async function GET() {
+//   const sitemapUrl =
+//     "https://storage.googleapis.com/perec-news.firebasestorage.app/sitemap-index.xml";
+
+//   try {
+//     const response = await fetch(sitemapUrl);
+
+//     if (!response.ok) {
+//       return new Response(
+//         `Fetch failed: ${response.status} ${response.statusText}`,
+//         {
+//           status: response.status,
+//         }
+//       );
+//     }
+
+//     const xml = await response.text();
+
+//     return new Response(xml, {
+//       headers: {
+//         "Content-Type": "application/xml",
+//         "Cache-Control": "no-cache",
+//       },
+//     });
+//   } catch (error) {
+//     return new Response("Error: " + error.message, {
+//       status: 500,
+//     });
+//   }
+// }
