8aa359fb721f871102ef5e5ec33c51f613ec2345
Oleg <Oleg@macs-MacBook-Pro-4.local>
2025-05-02 11:47:08 +0200

    sitemap

--- src/middleware.js
+++ src/middleware.js
@@ -1,11 +1,24 @@
-// src/middleware.js
+import { NextResponse } from "next/server";
 import createMiddleware from "next-intl/middleware";
 
-export default createMiddleware({
+const intlMiddleware = createMiddleware({
   locales: ["en", "ru"],
   defaultLocale: "ru",
 });
 
+export function middleware(req) {
+  const { pathname } = req.nextUrl;
+
+  if (pathname === "/sitemap.xml") {
+    return NextResponse.redirect(
+      "https://storage.googleapis.com/perec-news.firebasestorage.app/sitemap-index.xml",
+      307
+    );
+  }
+
+  return intlMiddleware(req);
+}
+
 export const config = {
-  matcher: ["/", "/(ru|en)/:path*"],
+  matcher: ["/", "/(ru|en)/:path*", "/sitemap.xml"],
 };
