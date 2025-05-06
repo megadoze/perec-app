803035dd4a605459e9add2c9b237994f68c4cc7e
Oleg <Oleg@macs-MacBook-Pro-4.local>
2025-05-06 18:23:07 +0200

    dkfjgbnb

--- src/lib/firebaseAdmin.js
+++ src/lib/firebaseAdmin.js
@@ -1,11 +1,14 @@
 import { initializeApp, cert, getApps } from "firebase-admin/app";
 import { getDatabase } from "firebase-admin/database";
-import serviceAccount from "../../serviceAccountKey.json"; // путь зависит от места, где ты импортируешь
 
 if (!getApps().length) {
   initializeApp({
-    credential: cert(serviceAccount),
-    databaseURL: "https://perec-news-default-rtdb.europe-west1.firebasedatabase.app",
+    credential: cert({
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+      privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+    }),
+    databaseURL: process.env.FIREBASE_DATABASE_URL,
   });
 }
 
