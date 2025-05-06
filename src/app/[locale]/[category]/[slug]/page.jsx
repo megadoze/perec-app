dfcba08a5872f0ae279d9fd91fda48dc43b74ac0
Oleg <Oleg@macs-MacBook-Pro-4.local>
2025-05-06 17:03:34 +0200

    change generateMetadata

--- src/app/[locale]/[category]/[slug]/page.jsx
+++ src/app/[locale]/[category]/[slug]/page.jsx
@@ -5,65 +5,80 @@ import { notFound } from "next/navigation";
 import NewsContent from "@/components/newsClient";
 import FadeWrapper from "@/components/fadeWrapper";
 
-export async function generateMetadata({ params }) {
-  const { slug, locale } = await params;
-
-  const id = slug?.split("-").at(-1);
-
-  const messages = (await import(`@/lang/${locale}/common.json`)).default;
-
-  const subTitleDesc = messages.metaTwitterDescription;
-  const siteName = messages.metaTitle;
-
-  const snapshot = await get(child(ref(db), `news/${id}`));
-  const news = snapshot.val();
-
-  const emptyNews = {
-    ru: "Новость не найдена",
-    en: "News not found",
-  };
-
-  if (!news) {
-    return { title: emptyNews[locale] };
-  }
-
-  const title = news.translations[locale].title;
-  const description = news.translations[locale].subTitle || subTitleDesc;
-  const url = `https://perec.news/${locale}/${news.category}/${news.translations[locale].slug}`;
-  const image =
-    Array.isArray(news?.images) && news.images.length > 0
-      ? news.images[0]
-      : "https://firebasestorage.googleapis.com/v0/b/perec-news.firebasestorage.app/o/public%2Fpublic_perec.webp?alt=media";
-
+export async function generateMetadata() {
   return {
-    title,
-    description,
+    title: "Test Title",
     openGraph: {
-      title,
-      description,
-      url,
-      siteName: siteName,
-      type: "article",
+      title: "Test OG Title",
+      description: "OG Description here",
       images: [
         {
-          url: image,
-          width: 1200,
-          height: 630,
-          alt: title,
+          url: "https://example.com/og-image.jpg",
         },
       ],
     },
-    twitter: {
-      card: "summary_large_image",
-      title,
-      description,
-      images: [image],
-    },
-    other: {
-      "x-debug-tag": "from-generateMetadata",
-    },
   };
 }
+
+// export async function generateMetadata({ params }) {
+//   const { slug, locale } = await params;
+
+//   const id = slug?.split("-").at(-1);
+
+//   const messages = (await import(`@/lang/${locale}/common.json`)).default;
+
+//   const subTitleDesc = messages.metaTwitterDescription;
+//   const siteName = messages.metaTitle;
+
+//   const snapshot = await get(child(ref(db), `news/${id}`));
+//   const news = snapshot.val();
+
+//   const emptyNews = {
+//     ru: "Новость не найдена",
+//     en: "News not found",
+//   };
+
+//   if (!news) {
+//     return { title: emptyNews[locale] };
+//   }
+
+//   const title = news.translations[locale].title;
+//   const description = news.translations[locale].subTitle || subTitleDesc;
+//   const url = `https://perec.news/${locale}/${news.category}/${news.translations[locale].slug}`;
+//   const image =
+//     Array.isArray(news?.images) && news.images.length > 0
+//       ? news.images[0]
+//       : "https://firebasestorage.googleapis.com/v0/b/perec-news.firebasestorage.app/o/public%2Fpublic_perec.webp?alt=media";
+
+//   return {
+//     title,
+//     description,
+//     openGraph: {
+//       title,
+//       description,
+//       url,
+//       siteName: siteName,
+//       type: "article",
+//       images: [
+//         {
+//           url: image,
+//           width: 1200,
+//           height: 630,
+//           alt: title,
+//         },
+//       ],
+//     },
+//     twitter: {
+//       card: "summary_large_image",
+//       title,
+//       description,
+//       images: [image],
+//     },
+//     other: {
+//       "x-debug-tag": "from-generateMetadata",
+//     },
+//   };
+// }
 
 export default async function NewsPage({ params }) {
   const { slug, locale } = await params;
