7beaa28fb0b8745e21cb7f9420a6f25534377987
Oleg <Oleg@macs-MacBook-Pro-4.local>
2025-05-06 21:41:06 +0200

    telegram

--- src/app/[locale]/[category]/[slug]/page.jsx
+++ src/app/[locale]/[category]/[slug]/page.jsx
@@ -6,7 +6,7 @@ import NewsContent from "@/components/newsClient";
 import FadeWrapper from "@/components/fadeWrapper";
 
 export async function generateMetadata({ params }) {
-  const { slug, locale } = params;
+  const { slug, locale } = await params;
 
   const id = slug?.split("-").at(-1);
 
