bedc69b0e8ea7acb063f1dac7f301b32675ba49e
Oleg <Oleg@macs-MacBook-Pro-4.local>
2025-05-09 00:04:49 +0200

    telega

--- src/app/[locale]/page.jsx
+++ src/app/[locale]/page.jsx
@@ -1,39 +1,39 @@
-// import { getTranslations } from "next-intl/server";
+import { getTranslations } from "next-intl/server";
 import ClientHome from "@/components/clientHome";
 import { getHomePageData } from "@/lib/getHomePageData";
 
-// export async function generateMetadata({ params }) {
-//   const { locale } = await params;
-//   const t = await getTranslations({ locale });
+export async function generateMetadata({ params }) {
+  const { locale } = await params;
+  const t = await getTranslations({ locale });
 
-//   return {
-//     title: t("metaTitle"),
-//     description: t("metaDescription"),
-//     openGraph: {
-//       title: t("metaTitle"),
-//       description: t("metaDescription"),
-//       url: `https://perec.news/${locale}`,
-//       siteName: t("metaTitle"),
-//       images: [
-//         {
-//           url: "https://firebasestorage.googleapis.com/v0/b/perec-news.firebasestorage.app/o/public%2Fpublic_perec.webp?alt=media",
-//           width: 1200,
-//           height: 630,
-//           alt: t("metaTitle"),
-//         },
-//       ],
-//       type: "website",
-//     },
-//     twitter: {
-//       card: "summary_large_image",
-//       title: t("metaTitle"),
-//       description: t("metaTwitterDescription"),
-//       images: [
-//         "https://firebasestorage.googleapis.com/v0/b/perec-news.firebasestorage.app/o/public%2Fpublic_perec.webp?alt=media",
-//       ],
-//     },
-//   };
-// }
+  return {
+    title: t("metaTitle"),
+    description: t("metaDescription"),
+    openGraph: {
+      title: t("metaTitle"),
+      description: t("metaDescription"),
+      url: `https://perec.news/${locale}`,
+      siteName: t("metaTitle"),
+      images: [
+        {
+          url: "https://firebasestorage.googleapis.com/v0/b/perec-news.firebasestorage.app/o/public%2Fpublic_perec.webp?alt=media",
+          width: 1200,
+          height: 630,
+          alt: t("metaTitle"),
+        },
+      ],
+      type: "website",
+    },
+    twitter: {
+      card: "summary_large_image",
+      title: t("metaTitle"),
+      description: t("metaTwitterDescription"),
+      images: [
+        "https://firebasestorage.googleapis.com/v0/b/perec-news.firebasestorage.app/o/public%2Fpublic_perec.webp?alt=media",
+      ],
+    },
+  };
+}
 
 export default async function HomePage({ params }) {
   const { locale } = await params;
