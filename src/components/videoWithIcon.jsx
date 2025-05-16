b56801fb485d6680f711fb3b9fcf079d68e55a68
Oleg <Oleg@macs-MacBook-Pro-4.local>
2025-05-16 13:56:37 +0200

    video title on mobile

--- src/components/videoWithIcon.jsx
+++ src/components/videoWithIcon.jsx
@@ -90,19 +90,17 @@ export default function VideoWithIcon({ imageUrl, posterUrl, news, locale }) {
         </div>
       )}
 
-      {!isMobile && (
-        <div
-          ref={titleRef}
-          className="absolute bottom-4 left-4 right-4 bg-gray-900/40 px-4 py-2 rounded-sm"
-          style={{ display: "flex" }}
-        >
-          <Link href={`/${locale}/media/${news.translations[locale].slug}`}>
-            <h2 className="text-2xl font-narrow tracking-wide text-white">
-              {news.translations[locale].title}
-            </h2>
-          </Link>
-        </div>
-      )}
+      <div
+        ref={!isMobile ? titleRef : null}
+        className="absolute bottom-4 left-4 right-4 bg-gray-900/40 px-4 py-2 rounded-sm"
+        style={!isMobile ? { display: "flex" } : undefined}
+      >
+        <Link href={`/${locale}/media/${news.translations[locale].slug}`}>
+          <h2 className="text-2xl font-narrow tracking-wide text-white">
+            {news.translations[locale].title}
+          </h2>
+        </Link>
+      </div>
     </div>
   );
 }
